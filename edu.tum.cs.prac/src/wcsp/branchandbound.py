ec92f5699a45f1be449eecf8f9b3241914bc67e3
danielnyga <nyga@in.tum.de>
2013-02-15 18:11:38 +0000

    intermediate commit

--- edu.tum.cs.prac/src/wcsp/branchandbound.py
+++ edu.tum.cs.prac/src/wcsp/branchandbound.py
@@ -20,6 +20,7 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from pracmln.PRACMLN import PRACMLN
 from pracmln.PRACDatabase import PRACDatabase
 from grammar import *
@@ -30,135 +31,96 @@ from utils import combinations
 
 class FormulaGrounding(object):
     
-    def __init__(self, formula, mrf, domains=None):
+    def __init__(self, formula, predicate, mrf):
         self.formula = formula
+        self.predicate = predicate
         self.mrf = mrf
         
-        if domains is None: # use the whole domain per default 
-            domains = {}
-            for var in formula.getVariables(mrf.mln):
-                domains[var] = mrf.domains[formula.getVarDomain(var, mrf.mln)]
-        self.var_domains = domains
-        
-    def groundWithGroundAtom(self, gndAtom, lastPredicate):
-        self.unifiers = []
-        for lit in self.formula.iterLiterals():
-            unif = FormulaGrounding.getAtomUnifier(lit, gndAtom)
-            if unif is not None:
-                self.updateUnifiers(unif)
-        print self.formula,
-        print self.unifiers,
-        if len(self.formula.getVariables(self.mrf.mln)) == 0 or (len(self.unifiers) == 0 and not lastPredicate):
-            yield FormulaGrounding(self.formula, self.mrf)
-        elif len(self.unifiers) == 1:
-            self.unifiers = [self.unifiers]
-        else:
-            self.unifiers = [(u,{}) for u in self.unifiers]
-        for unifier in combinations(self.unifiers):
-            unif = {}
-            for u in unifier: unif.update(u)
-            if len(unif) == 0: continue
-            f = self.formula.ground(self.mrf, unif, simplify=False, allowPartialGroundings=True)
-            yield FormulaGrounding(f, self.mrf)
+        # determine the independent variable sets
+        seen_vars = set()
+        variable_sets = []
+        literals = []
+        seen_assignments = []
+        ground_formulas = []
+        for literal in formula.iterLiterals():
+            if not literal.predName == self.predicate: continue
+            vars = set(literal.getVariables(mrf).keys())
+            literals.append(literal)
+            unseen_vars = vars.difference(seen_vars)
+            if len(unseen_vars) > 0:
+                variable_sets.append(unseen_vars)
+                seen_assignments.append([])
+            seen_vars.update(unseen_vars)
+        self.literals = literals
+        self.variable_sets = variable_sets
+        self.seen_assignments = seen_assignments
             
-    @staticmethod
-    def getAtomUnifier(lit, gndAtom):
-        predName = lit.predName if type(lit) is Lit else lit.gndAtom.predName
-        params = lit.params if type(lit) is Lit else lit.gndAtom.params
-        if predName != gndAtom.predName:
-            return None
-        unification = {}
-        for p1, p2 in zip(params, gndAtom.params):
-            if isVar(p1):
-                unification[p1] = p2
-            elif p1 != p2:
-                return None
-        return unification
-    
-    def updateUnifiers(self, unifier):
-        unifiersToMerge = []
-        self.unifiers.append(unifier)
-        for i, u in enumerate(self.unifiers):
-            if set(u).issubset(set(unifier)) or set(unifier).issubset(set(u)):
-                unifiersToMerge.append(i)
-#        print self.unifiers
-        if len(unifiersToMerge) == 0:
+            
+    def ground(self, gndAtom):
+        new_assignments = []
+        for i_lit, lit in enumerate(self.literals):
+            litAssignment = self.gndAtom2Assignment(lit, gndAtom)
+            if litAssignment is not None:
+                assignment = dict([(var, litAssignment[var]) for var in self.variable_sets[i_lit]])
+                new_assignments.append(assignment)
+#        print 'old', self.seen_assignments
+#        print 'new', new_assignments
+        if len(new_assignments) == 0:
+            return 
+#        if len(self.seen_assignments) == 0:
+#            self.iterGroundings([1]*len(self.variable_sets), new_assignments)
+#        else:
+        for c in combinations([[0,1]]*len(self.variable_sets)):
+            if sum(c) == 0: continue
+            print c    
+            self.iterGroundings(c, new_assignments)
+        for i_lit, lit in enumerate(self.literals):
+#            print 'adding', new_assignments[i_lit], 'to', self.seen_assignments[i_lit]
+            self.seen_assignments[i_lit].append(new_assignments[i_lit])
+        
+            
+    def iterGroundings(self, oldOrNew, newAssignments, assignment=None):
+        if assignment is None:
+            assignment = {}
+        if len(oldOrNew) == 0:
+#            print assignment
+            f = self.formula.ground(mrf, assignment, allowPartialGroundings=True)
+            print f
+            self.groundFormulas.append()
             return
-        mergedUnifier = {}
-        for i in unifiersToMerge:
-            mergedUnifier.update(self.unifiers[i])
-        self.unifiers = [u for i, u in enumerate(self.unifiers) if i not in unifiersToMerge]
-        self.unifiers.append(mergedUnifier)
-
-    def __str__(self):
-        return str(self.formula)
-
-class LazyBnB(object):
+        if oldOrNew[0]:
+            possibleAssignments = [newAssignments[len(oldOrNew) - 1]]
+        else:
+            possibleAssignments = self.seen_assignments[len(oldOrNew) - 1]
+        for assign in possibleAssignments:
+            assign = dict(assign)
+            assign.update(assignment)
+            self.iterGroundings(oldOrNew[1:], newAssignments, assign)
+        
+            
+    def gndAtom2Assignment(self, lit, atom):
+        '''
+        Returns None if the literal and the atom do not match.
+        '''
+#        print 'lit:', lit
+        if lit.predName != atom.predName: return None
+        assignment = {}
+        for p1, p2 in zip(lit.params, atom.params):
+            if FOL.isVar(p1):
+                assignment[p1] = p2
+#            elif p1 != p2: return None
+        return assignment
     
-    def __init__(self, mrf):
-        self.mrf = mrf
-        
-    def solve(self):
-        mrf = self.mrf
-        gndAtoms = mrf.gndAtoms
-        newFormulaGroundings = [FormulaGrounding(f, mrf) for f in mrf.formulas]
-        formulaGroundings = None
-        recentPredicate = None
-        reruns = {}
-        predicates = list(mrf.mln.predicates)
-        for gndAtom in sorted(gndAtoms, reverse=True) + [None]:
-            gndAtom = gndAtoms.get(gndAtom, None)
-            if gndAtom is None or recentPredicate != gndAtom.predName:
-                if len(reruns) > 0:
-                    print 'rerunning'
-                    newNewFormulaGroundings = []
-                    atomList = list(reruns)
-                    print atomList[0]
-                    for reruni, rerun in enumerate(atomList):
-                        for ai, atom in enumerate([a for a in gndAtoms.values() if a.predName == recentPredicate]):
-                            print 'enter loop'
-                            print map(str,reruns[rerun])
-#                            if ai <= reruni: continue
-                            print atom
-                            for i, fg in enumerate(reruns[rerun]):
-                                print fg
-                                for gf in fg.groundWithGroundAtom(atom, True):
-                                    print '-> adding'
-                                    newNewFormulaGroundings.append(gf)
-#                        newNewFormulaGroundings.extend(reruns[rerun])
-                    newFormulaGroundings.extend(newNewFormulaGroundings)
-                    reruns = {}
-                    print 'stop rerunning'
-                formulaGroundings = newFormulaGroundings
-                if gndAtom is None:
-                    break
-                if gndAtom.predName in predicates:
-                    predicates.remove(gndAtom.predName)
-                recentPredicate = gndAtom.predName
-                newFormulaGroundings = []
-            for fg in formulaGroundings:
-                newOnes = []
-                for gf in fg.groundWithGroundAtom(gndAtom, len(predicates) == 0):
-                    print '-> adding'
-                    newOnes.append(gf)
-                if len(newOnes) > 1: 
-                    reruns[gndAtom] = newOnes[1:]
-                    newFormulaGroundings.append(newOnes[0])
-                else:
-                    newFormulaGroundings.extend(newOnes)
-        formulaGroundings = newFormulaGroundings
-        
-        print 'FINAL'
-        for gf in formulaGroundings:
-            print gf
-                
+    
+    
+    
 if __name__ == '__main__':
     
     mln = PRACMLN()
     mln.declarePredicate('foo', ['x', 'y'])
     mln.declarePredicate('bar', ['y','z'])
     
-    f = parsePracFormula('foo(?x1,Daniel) ^ foo(X,?y2) => bar(?y2,?z)')
+    f = parsePracFormula('foo(?x1,?y1) ^ foo(?x2,Daniel) => foo(?y3,?z)')
     mln.addFormula(f, 1.5)
     
     db = PRACDatabase(mln)
@@ -168,24 +130,17 @@ if __name__ == '__main__':
     db.addGroundAtom('bar(Bob, Y)')
     
     mrf = mln.groundMRF(db, simplify=False)
-    print sorted(mrf.gndAtoms, reverse=True)
+    
     print mrf.domains
-    bnb = LazyBnB(mrf)
-    bnb.solve()
+    fg = FormulaGrounding(f, 'foo', mrf)
     
-#    print 'evidence:'
-#    for e, t in zip(mrf.gndAtoms, mrf.evidence):
-#        print e, t 
-#    fg = FormulaGrounding(f, mrf)
-#    for gndAtom in mrf.gndAtoms.values():
-#        fg.groundWithGroundAtom(gndAtom)
-        
-#    gf = fg.formula.ground(mrf, {'?x': 'X', '?z': 'Z', '?y': 'Fred'}, simplify=True, allowPartialGroundings=True)
-#    gf.printStructure()
-#    print 'grounding:', gf,
-#    print 'is', gf.isTrue(mrf.evidence)
-    
-    
+    previous = None
+    for a in sorted(mrf.gndAtoms):
+        a = mrf.gndAtoms[a]
+        if a != previous:
+            pass
+        print 'atom:', a
+        fg.ground(a)
 
 
 
