5ef9d697ea0bb010e1048e212a73c38ce7e8c925
danielnyga <nyga@in.tum.de>
2013-06-04 09:11:48 +0000

    adapted make_apps.py to new repo structure

--- make_apps.py
+++ make_apps.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import runpy
-sys.path.append(os.path.join('edu.tum.cs.prac', 'src'))
+sys.path.append(os.path.join('praccore', 'src'))
 from utils import bash
 from subprocess import Popen
 home = os.path.abspath(".")
@@ -14,7 +13,7 @@ for app in ("pracinfer", "praclearn", 'pracparse', 'senses', 'pracserver'):
     f = file(filepath, "w+")
     f.write("#!/bin/sh\n")
     f.write('cd %s\n' % home)
-    f.write("python %s \"$@\"\n" % os.path.join(home, "edu.tum.cs.prac", "src", "%s.py" % app))
+    f.write("python %s \"$@\"\n" % os.path.join(home, "praccore", "src", "%s.py" % app))
     f.close()
     print '\t Wrote app %s%s%s' % (bash.BOLD, app, bash.END)
     os.system("chmod a+x %s" % filepath)
@@ -37,13 +36,13 @@ print '\t Building %spyjamas%s...' % (bash.BOLD, bash.END)
 cwd = os.path.join('3rdparty', 'pyjs')
 Popen('python bootstrap.py', shell=True, cwd=cwd).wait()
 print '\t Building %spracviz%s...' % (bash.BOLD, bash.END)
-cmd = 'PYJSPATH=$PYJSPATH:../../edu.tum.cs.prac/src %s --output=%s pracviz.py' % (os.path.join('..', '..', '3rdparty', 'pyjs', 'bin', 'pyjsbuild'), os.path.join('output'))
+cmd = 'PYJSPATH=$PYJSPATH:../../praccore/src %s --output=%s pracviz.py' % (os.path.join('..', '..', '3rdparty', 'pyjs', 'bin', 'pyjsbuild'), os.path.join('output'))
 print cmd
 Popen(cmd, cwd=os.path.join('pracviz', 'src'), shell=True).wait()
 
 f = file("env.sh", "w+")
 f.write("#!/bin/sh\n")
-f.write("export PYTHONPATH=%s\n" % os.path.pathsep.join(["$PYTHONPATH", os.path.join(home, "edu.tum.cs.prac", "src"), 
+f.write("export PYTHONPATH=%s\n" % os.path.pathsep.join(["$PYTHONPATH", os.path.join(home, "praccore", "src"), 
                                                         os.path.join(home, "semcore_annotation_tool", "src"), os.path.join('3rdparty', 'webpy_jsonrpc')]))
 f.write("export PATH=%s\n" % os.path.pathsep.join(["$PATH", os.path.join(home, appfolder)]))
 f.write("export PRAC_HOME=%s\n" % home)
