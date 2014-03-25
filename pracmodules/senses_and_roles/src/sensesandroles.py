79bc75c68c444d738b0102d81b9dc06205184df8
Daniel Nyga <nyga@cs.uni-bremen.de>
2014-03-25 09:38:22 +0100

    Update on senses and roles module.

--- pracmodules/senses_and_roles/src/sensesandroles.py
+++ pracmodules/senses_and_roles/src/sensesandroles.py
@@ -22,14 +22,14 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from prac.core import PRACModule, PRACPIPE, PRACKnowledgeBase
-from actioncore.inference import PRACInferenceStep
 import logging
 from mln import readMLNFromFile, readDBFromFile#, MLNParsingError
-from mln.methods import ParameterLearningMeasures
+from mln.methods import LearningMethods
 import sys
 from wcsp.converter import WCSPConverter
 from mln.database import Database
 import os
+from prac.inference import PRACInferenceStep
 
 
 class SensesAndRoles(PRACModule):
@@ -37,93 +37,44 @@ class SensesAndRoles(PRACModule):
     
     '''
     
-    stanford_parser = None
-    
     def initialize(self):
-        # load the action core-specific MLNs
-        self.mlns = {}
-        log = logging.getLogger(self.name)
-        for action_core in self.prac.action_cores:
-            try:
-                mln = readMLNFromFile(os.path.join(self.module_path, 'mln', '%s.mln' % action_core), grammar='PRACGrammar', logic='FuzzyLogic')
-                log.info(mln.predicates)
-                self.mlns[action_core] = mln
-            except Exception, e:# Exception:#MLNParsingError:
-                log.error(e.message)
-                log.error('No MLN found for "%s".' % action_core)
-
+        pass
+    
     def shutdown(self):
         pass
     
     @PRACPIPE
-    def infer(self, pracinference):
+    def __call__(self, pracinference, **params):
         log = logging.getLogger(self.name)
-        inf_step = PRACInferenceStep(pracinference, self)
-        for input_db in pracinference.module2infSteps['ac_recognition'][0].output_dbs:
-            for s in input_db.query('action_core(?ac)'):
-                action_core_name = s['?ac']
-            if action_core_name is None:
-                log.exception('Action core cannot be found. Run the ac_recognition module first.')
-            log.info('Loading microtheory for %s' % action_core_name)
-            microtheory = self.load_pracmt(action_core_name)
-            mln = microtheory.learned_mln
-            query_db = input_db.duplicate(mln, ignoreUnknownPredicates=True)
-            log.info(mln.domains['concept'])
-            # filter out unknown concepts
-            log.info(mln.domains)
-            for _, lit in input_db.iterGroundLiteralStrings('is_a'):
-                _, _, params = mln.logic.parseLiteral(lit)
-                concept = params[1]
-                if not concept in mln.domains['concept']:
-                    query_db.retractGndAtom(lit)
-            log.info('database evidence')
-            query_db.printEvidence()
-            action_core_name = None
-            mln.setClosedWorldPred(*[p for p in microtheory.learned_mln.predicates if p not in ('has_sense', 'action_role')])
-            mln.write(sys.stdout, color=True)
-            mrf = mln.groundMRF(query_db)
-            converter = WCSPConverter(mrf)
-            result_db = converter.getMostProbableWorldDB()
-            for res in result_db.query('has_sense(?w,?s) ^ action_role(?w,?r)'):
-                print res
-        return inf_step
-                
-    @PRACPIPE
-    def train(self, praclearn):
-        log = logging.getLogger('PRAC')
-        prac = self.prac
-        mts = []
-        if praclearn.microtheories is None:
-            mts = prac.action_cores
+        kb = params.get('kb', None)
+        if kb is None:
+            # load the default arguments
+            dbs = pracinference.inference_steps[-1].output_dbs
         else:
-            mts = praclearn.microtheories
-        wn_senses = prac.getModuleByName('wn_senses')
-        for mt in mts:
-            db_files = prac.getActionCoreTrainingDBs(mt)
-            # add the fuzzy class hierarchy evidences
-            training_dbs = []
-            mln = self.mlns[mt]
-            for db in db_files: 
-                db = readDBFromFile(mln, db)
-                training_dbs.append(db)
-            wn_senses.addFuzzyEvidenceToDBs(*training_dbs)
-            learnedMT = SensesAndRolesMT(self, mt)
-            learnedMT.train(training_dbs)
-            self.save_pracmt(learnedMT)
-            
-    
-class SensesAndRolesMT(PRACKnowledgeBase):
-    
-    def train(self, training_dbs):
-        log = logging.getLogger('PRAC')
-        mln = self.module.mlns[self.name]
-        self.training_dbs = training_dbs
-        mln.write(sys.stdout, color=True)
-        queryPreds = list(mln.predicates)
-        queryPreds.remove('is_a')
-        self.learned_mln = mln.learnWeights(training_dbs, ParameterLearningMeasures.BPLL_CG, 
-                                            optimizer='bfgs', verbose=True, 
-                                            maxSteps=100, learningRate=0.01, queryPreds=queryPreds)#, gaussianPriorSigma=1.)
+            kb = params['kb']
+            dbs = kb.dbs
+        
+        inf_step = PRACInferenceStep(pracinference, self)
+        for db in dbs:
+            db_ = db.duplicate()
+            for q in db.query('action_core(?w, ?ac)'):
+                actioncore = q['?ac']
+                if kb is None:
+                    useKB = self.load_pracmt(actioncore)
+                else:
+                    useKB = kb
+            roles = useKB.query_mln.domains.get('role', [])
+            specified_roles = []
+            for q in db.query('action_role(?w, ?r)'):
+                specified_roles.append(q['?r'])
+            unknown_roles = set(roles).difference(specified_roles)
+            for i, role in enumerate(unknown_roles):
+                if role == 'null': continue
+                db_.addGroundAtom('action_role(skolem-%s, %s)' % (role, role))
+            db_.write(sys.stdout, color=True)
+#                 result = list(useKB.infer(db))
+#             inf_step.output_dbs.extend(result_db)
+        return inf_step
         
         
         
