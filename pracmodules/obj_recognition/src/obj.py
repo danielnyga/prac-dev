26d0c5b65bcc2a258be2a35af570299d4c5a795b
Mareike <mareikep@tzi.de>
2014-07-24 11:50:44 +0200

    removed previous inference steps

--- pracmodules/obj_recognition/src/obj.py
+++ pracmodules/obj_recognition/src/obj.py
@@ -21,13 +21,13 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from prac.core import PRACModule, PRACKnowledgeBase, PRACPIPE
-from mln import readMLNFromFile, readDBFromFile, Database
+from prac.core import PRACModule, PRACKnowledgeBase, PRACPIPE, DescriptionKnowledgeBase
+from mln import readMLNFromFile, readMLNFromString, readDBFromFile, Database
 import logging
 from mln.methods import LearningMethods
 from prac.wordnet import WordNet
 from prac.inference import PRACInferenceStep
-import sys
+import sys, os
 from utils import colorize
 
 class NLObjectRecognition(PRACModule):    
@@ -44,8 +44,12 @@ class NLObjectRecognition(PRACModule):
         print colorize('| PRAC OBJECT RECOGNITION: RECOGNIZING OBJECTS|', (None, 'green', True), True)
         print colorize('+=============================================+', (None, 'green', True), True)
         print
-        print colorize('Inferring most probable ANNOTATION + simultaneous WORD SENSE DISMABIGUATION...', (None, 'white', True), True)
+        print colorize('Inferring most probable object based on nl description properties...', (None, 'white', True), True)
         
+        dkb = params.get('dkb')
+        print 'using knowledge base: ', dkb.descriptions
+        print 
+
         if params.get('kb', None) is None:
             # load the default arguments
             dbs = pracinference.inference_steps[-1].output_dbs
@@ -63,53 +67,19 @@ class NLObjectRecognition(PRACModule):
         
         # process databases
         for db in kb.dbs:
-            print colorize('+============DB=================+', (None, 'green', True), True)
-            db.write(sys.stdout,color=True)
-
-            db = wordnet_module.get_senses_and_similarities(db, known_concepts)
-            print colorize('+============DB after get senses and similarities=================+', (None, 'green', True), True)
-            db.write(sys.stdout,color=True)
-            
-            # add cluster to domains
-            if 'cluster' in db.domains:
-                domains = db.domains['cluster']
-                domains.append('cluster')
-            else:
-                db.domains['cluster'] = ['cluster']
+            #db.write(sys.stdout, color=True)
+            print 
+            print
 
             # infer and update output dbs
             result_db = list(kb.infer(db))
             inf_step.output_dbs.extend(result_db)
 
             for r_db in result_db:
-                print colorize('+============Result DB=================+', (None, 'green', True), True)
-                r_db.write(sys.stdout, color=True)
-                
                 # print annotations found in result db
-                for annot in ['has_a', 'color', 'shape', 'size']:
-                    for q in r_db.query('{}(?cluster, ?sense)'.format(annot)):
-                        if q['?sense'] == 'null': continue
-                        print '{}({},{})'.format(colorize(annot, (None, 'yellow', True), True)  , q['?cluster'], colorize(q['?sense'], (None, 'white', True), True))
-                    print
-
-                for annot in ['is_a', 'coRef']:
-                    for q in r_db.query('{}(?sense, ?concept)'.format(annot)):
-                        if q['?concept'] == 'null': continue
-                        print '{}({},{})'.format(colorize(annot, (None, 'yellow', True), True)  , q['?concept'], colorize(q['?concept'], (None, 'white', True), True))
- 
                 for q in r_db.query('object(?cluster, ?concept)'):
                     if q['?concept'] == 'null': continue
-                    print '{}({},{})'.format(colorize('object', (None, 'yellow', True), True)  , q['?cluster'], colorize(q['?concept'], (None, 'white', True), True))
-
-                print 'Inferred most probable word senses:'
-                wordSenses = {}
-                for q in r_db.query('has_sense(?w, ?s)'):
-                    if q['?s'] == 'null': continue
-                    print '{}:'.format(q['?w'])
-                    wordSenses[q['?w']] = q['?s']
-                    wordnet_module.printWordSenses(wordnet_module.get_possible_meanings_of_word(r_db, q['?w']), q['?s'])
-                    print
-                # print wordSenses
+                    print 'object({}, {})'.format(q['?cluster'], colorize(q['?concept'], (None, 'white', True), True))
         return inf_step
 
 
@@ -119,15 +89,17 @@ class NLObjectRecognition(PRACModule):
         print colorize('+=============================================+', (None, 'green', True), True)
 
         log = logging.getLogger('prac')
-        # prac = praclearning.prac
-        # get all the relevant training databases
-        # kb = self.load_pracmt('obj_recog')
-        # mln = kb.query_mln
         logging.getLogger().setLevel(logging.DEBUG)
+        # prac = praclearning.prac
         
-        mln = readMLNFromFile('/home/mareikep/prac_repos/prac/pracmodules/obj_recognition/mln/parsing.mln')
-        dbFile = '/home/mareikep/prac_repos/prac/pracmodules/obj_recognition/mln/ts_stanford_wn_man.db'
-        outputfile = '/home/mareikep/prac_repos/prac/pracmodules/obj_recognition/mln/bpll_cg_parsing_stanford_wn_man.mln'
+        # get all the relevant training databases
+        kb = self.load_pracmt('obj_recog')
+
+        # mln = readMLNFromString(kb.query_mln_str)
+        mln = readMLNFromFile(os.path.join(self.module_path, 'mln/objectinference.mln'))
+
+        dbFile = os.path.join(self.module_path, 'db/ts_stanford_wn_man.db')
+        outputfile = os.path.join(self.module_path, 'mln/dcll_objectinference_stanford_wn_man.mln')
         inputdbs = readDBFromFile(mln, dbFile)
         
         known_concepts = mln.domains.get('concept', [])
@@ -142,7 +114,7 @@ class NLObjectRecognition(PRACModule):
         log.info('Starting training with {} databases'.format(len(training_dbs)))
         # trainedMLN = mln.learnWeights(training_dbs, LearningMethods.BPLL_CG, partSize=8, gaussianPriorSigma=10, verbose=False, optimizer='bfgs')
         # trainedMLN = mln.learnWeights(training_dbs, LearningMethods.DBPLL_CG, evidencePreds=['is_a'],  partSize=4, verbose=False, optimizer='bfgs')
-        trainedMLN = mln.learnWeights(training_dbs, LearningMethods.DCLL, evidencePreds=['is_a'], partSize=1, verbose=False, optimizer='bfgs')
+        trainedMLN = mln.learnWeights(training_dbs, LearningMethods.DCLL, evidencePreds=['is_a', 'has_sense', 'property','has_pos'], partSize=1, verbose=False, optimizer='bfgs')
         trainedMLN.write(file(outputfile, "w"))
         
         print colorize('+=============================================+', (None, 'green', True), True)
@@ -166,8 +138,8 @@ class AnnotationKB(PRACKnowledgeBase):
     '''
     
     def train(self, training_dbs):
-        mln = readMLNFromFile('/home/mareikep/prac_repos/prac/pracmodules/obj_recognition/mln/parsing.mln')
+        mln = readMLNFromFile(os.path.join(self.module_path, 'mln/parsing.mln'))
         self.dbs = training_dbs
         self.learn_mln = mln.learnWeights(training_dbs, LearningMethods.BPLL_CG, verbose=True, optimizer='bfgs')
-        outputfile = '/home/mareikep/prac_repos/prac/pracmodules/obj_recognition/mln/wts_pll_ts_stanford_wn_man.db'
+        outputfile = os.path.join(self.module_path, 'mln/wts_pll_ts_stanford_wn_man.db')
         # self.learn_mln.write(file(outputfile, "w"))
