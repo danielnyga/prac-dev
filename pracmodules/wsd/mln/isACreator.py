bb9518143bb85bfd43c846cc9a180a12dbf9966e
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2014-07-28 20:56:20 +0200

    Fixed isACreator.py

--- pracmodules/wsd/mln/isACreator.py
+++ pracmodules/wsd/mln/isACreator.py
@@ -1,14 +1,16 @@
+from mln.database import readDBFromFile, Database
+from mln.mln import readMLNFromFile
+from mln.util import mergeDomains
 from prac.core import PRAC
 from prac.wordnet import WordNet
-from mln.mln import readMLNFromFile
-from mln.database import readDBFromFile, Database
 import sys
-def workUpDb(db,f):
-        myList = list(db.query(query))
-        for result in myList:
+
+def workUpDb(db,f,concepts):
+        query = 'has_sense(?w,?s)'
+        for result in db.query(query):
             word = result['?s']
-            for res in myList:
-                db.addGroundAtom('is_a('+word+","+res['?s']+')',prac.wordnet.wup_similarity(word,res['?s']))
+            for concept in concepts:
+                db.addGroundAtom('is_a('+word+","+concept+')',prac.wordnet.wup_similarity(word,concept))
         db.write(f)
 
 if __name__ == '__main__':
@@ -21,12 +23,12 @@ if __name__ == '__main__':
     senses.initialize()
     mln = readMLNFromFile(mlnPath)
     dbs = readDBFromFile(mln,dbPath)
-    query = 'has_sense(?w,?s)'
-    i = -1;
-    f = open("out.db", 'w+')
+    
+    domains = map(lambda db: db.domains,dbs)
+    dictDomains = mergeDomains(*domains)
+    f = open('out.db','w+')
     for db in dbs[:-1]:
-        workUpDb(db, f)
+        workUpDb(db,f,dictDomains['sense'])
         f.write('---\n')
-    lastDb = dbs[-1]
-    workUpDb(lastDb,f)
-    f.close();            
+    workUpDb(dbs[-1],f,dictDomains['sense'])
+    f.close()    
