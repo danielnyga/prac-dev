1a1c1ad656b37876662839053dabe063d49c1f78
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2014-08-08 14:29:02 +0200

    Did some improvements in isACreator.

--- pracmodules/wsd/mln/isACreator.py
+++ pracmodules/wsd/mln/isACreator.py
@@ -5,34 +5,44 @@ from prac.core import PRAC
 from prac.wordnet import WordNet
 import sys
 
-def workUpDb(db,f,concepts):
+def workUpDb(db,f,concepts,fol=True):
         query = 'has_sense(?w,?s)'
         for result in db.query(query):
             word = result['?s']
-            #TODO add parameter which defines which is_a concepts should be created
-            ###FUZZY###
-            #for concept in concepts:
-                #db.addGroundAtom('is_a('+word+","+concept+')',prac.wordnet.wup_similarity(word,concept))
-            ###FOL###
-            db.addGroundAtom('is_a('+word+","+word+')')
+            if(fol):
+                db.addGroundAtom('is_a('+word+","+word+')')
+            else:
+                for concept in concepts:
+                    db.addGroundAtom('is_a('+word+","+concept+')',prac.wordnet.wup_similarity(word,concept))
         db.write(f)
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    mlnPath = args[0]
-    dbPath = args[1]
     prac = PRAC()
     prac.wordnet = WordNet(concepts=None)
     senses = prac.getModuleByName('wn_senses')
     senses.initialize()
+    fol = True
+    
+    args = sys.argv[1:]
+    if(args[0] == '--fuzzy'):
+        fol = False;
+        mlnPath = args[1]
+        dbPath = args[2]
+    else:
+        mlnPath = args[0]
+        dbPath = args[1]
+    
     mln = readMLNFromFile(mlnPath)
     dbs = readDBFromFile(mln,dbPath)
     
     domains = map(lambda db: db.domains,dbs)
     dictDomains = mergeDomains(*domains)
-    f = open('out.db','w+')
+    if(fol):
+        f = open('fol_training.db','w+')
+    else:
+        f = open('fuzzy_training.db','w+')
     for db in dbs[:-1]:
-        workUpDb(db,f,dictDomains['sense'])
+        workUpDb(db,f,dictDomains['sense'],fol)
         f.write('---\n')
-    workUpDb(dbs[-1],f,dictDomains['sense'])
+    workUpDb(dbs[-1],f,dictDomains['sense'],fol)
     f.close()    
