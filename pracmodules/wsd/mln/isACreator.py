30990a699b986f7fd3ab379b7a4904cb25081bc7
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2014-12-12 12:58:06 +0100

    Adjust isACreator to make it possible to use it in python code.

--- pracmodules/wsd/mln/isACreator.py
+++ pracmodules/wsd/mln/isACreator.py
@@ -5,44 +5,62 @@ from prac.core import PRAC
 from prac.wordnet import WordNet
 import sys
 
-def workUpDb(db,f,concepts,fol=True):
-        query = 'has_sense(?w,?s)'
+def workUpDb(db,concepts,queryPredicate,conceptVariable,prac,fol=True):
+        query = queryPredicate
         for result in db.query(query):
-            word = result['?s']
+            word = result[conceptVariable]
             if(fol):
                 db.addGroundAtom('is_a('+word+","+word+')')
             else:
                 for concept in concepts:
                     db.addGroundAtom('is_a('+word+","+concept+')',prac.wordnet.wup_similarity(word,concept))
-        db.write(f)
+        return db
 
-if __name__ == '__main__':
+def createIsAEvidence(mln,dbs,queryDomain,queryPredicate,conceptVariable,fol=True):
     prac = PRAC()
     prac.wordnet = WordNet(concepts=None)
     senses = prac.getModuleByName('wn_senses')
     senses.initialize()
-    fol = True
     
+    domains = map(lambda db: db.domains,dbs)
+    dictDomains = mergeDomains(*domains)
+    dbs_ = []
+    
+    for db in dbs:
+        dbs_.append(workUpDb(db,dictDomains[queryDomain],queryPredicate,conceptVariable,prac,fol))
+    
+    return dbs_
+
+if __name__ == '__main__':
     args = sys.argv[1:]
     if(args[0] == '--fuzzy'):
         fol = False;
         mlnPath = args[1]
         dbPath = args[2]
+        queryDomain = args[3]
+        queryPredicate = args[4]
+        conceptVariable = args[5]
     else:
+        fol = True
         mlnPath = args[0]
         dbPath = args[1]
+        queryDomain = args[2]
+        queryPredicate = args[3]
+        conceptVariable = args[4]
     
     mln = readMLNFromFile(mlnPath)
     dbs = readDBFromFile(mln,dbPath)
     
-    domains = map(lambda db: db.domains,dbs)
-    dictDomains = mergeDomains(*domains)
+    
     if(fol):
         f = open('fol_training.db','w+')
     else:
         f = open('fuzzy_training.db','w+')
+    
+    dbs_ = createIsAEvidence(mln, dbs, queryDomain, queryPredicate, conceptVariable, fol)    
+    
     for db in dbs[:-1]:
-        workUpDb(db,f,dictDomains['sense'],fol)
+        db.write(f)
         f.write('---\n')
-    workUpDb(dbs[-1],f,dictDomains['sense'],fol)
+    dbs[-1].write(f)
     f.close()    
