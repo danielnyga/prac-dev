3764cc5f9c0802da27a05ff44cd075922ed8a098
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2015-03-25 16:45:16 +0100

    Init achieved module.

---
+++ pracmodules/achieved_by/src/achievedBy.py
@@ -0,0 +1,58 @@
+# PROBABILISTIC ROBOT ACTION CORES 
+#
+# (C) 2012 by Daniel Nyga (nyga@cs.tum.edu)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from prac.core import PRACModule, PRACPIPE, PRACKnowledgeBase
+import logging
+from mln import readMLNFromFile, readDBFromFile#, MLNParsingError
+from mln.methods import LearningMethods
+import sys
+from wcsp.converter import WCSPConverter
+from mln.database import Database
+import os
+from prac.inference import PRACInferenceStep
+from mln.util import mergeDomains
+from utils import colorize
+from pracutils import printListAndTick
+
+
+class AchievedBy(PRACModule):
+    '''
+    
+    '''
+    
+    def initialize(self):
+        pass
+    
+    def shutdown(self):
+        pass
+    
+    @PRACPIPE
+    def __call__(self, pracinference, **params):
+        log = logging.getLogger(self.name)
+        print 'Started achievedby module'
+        
+    
+        
+    
+    
+    
