7828a27b8f268c038a08c49776299d96c867da15
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2015-03-25 14:33:05 +0100

    Updated bash script for running xval

--- pracmodules/ac_recognition_v2/src/Slicing/runXval.sh
+++ pracmodules/ac_recognition_v2/src/Slicing/runXval.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "Started senses xval"          
-python xvalACSensesRole.py -k 10 -m has_sense sense Template.mln Data.db 
+echo "Started senses xval"
+path=$(pwd)          
+python xvalACSensesRole.py -k 10 -m -f $path/Senses-Result has_sense sense Template.mln Data.db 
 wait $!
 echo "Finished senses xval"
 echo "Started roles xval"
-python xvalACSensesRole.py -k 10 -m action_role role Template.mln Data.db
+python xvalACSensesRole.py -k 10 -m -f $path/Roles-Result action_role role Template.mln Data.db
 wait $!
 echo "Finished roles xval"
