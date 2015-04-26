11d70cc16e5abb6b3754e14c3f4a160f1c21eff4
Mareike <mareikep@tzi.de>
2015-04-26 16:20:55 +0200

    merge qooxdoo and flask init + add qooxdoo sdk

---
+++ 3rdparty/qooxdoo-4.1-sdk/test/Gruntfile.js
@@ -0,0 +1,38 @@
+// requires
+var util = require('util');
+var qx = require("../tool/grunt");
+
+// grunt
+module.exports = function(grunt) {
+  var config = {
+
+    generator_config: {
+      let: {
+      }
+    },
+
+    common: {
+      "APPLICATION" : "test",
+      "QOOXDOO_PATH" : "..",
+      "LOCALES": ["en"],
+      "QXTHEME": "test.theme.Theme"
+    }
+
+    /*
+    myTask: {
+      options: {},
+      myTarget: {
+        options: {}
+      }
+    }
+    */
+  };
+
+  var mergedConf = qx.config.mergeConfig(config);
+  // console.log(util.inspect(mergedConf, false, null));
+  grunt.initConfig(mergedConf);
+
+  qx.task.registerTasks(grunt);
+
+  // grunt.loadNpmTasks('grunt-my-plugin');
+};
