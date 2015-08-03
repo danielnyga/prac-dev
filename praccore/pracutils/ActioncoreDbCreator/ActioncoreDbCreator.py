a3d9fe806e8276e9604c103e247d5390161371b9
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2015-08-03 14:21:24 +0200

    ActioncoreDbCreator extracts verbs of db.

--- praccore/pracutils/ActioncoreDbCreator/ActioncoreDbCreator.py
+++ praccore/pracutils/ActioncoreDbCreator/ActioncoreDbCreator.py
@@ -1,13 +1,15 @@
 import os
 import re
+import sys
 from prac.wordnet import WordNet
 
 class ActionCoreDbCreator(object):
     PRAC_HOME = os.environ['PRAC_HOME']
     ACTIONCORE_DB_PATH = os.path.join(PRAC_HOME, 'praccore', 'pracutils','ActioncoreDbCreator',"actioncore_dbs")
+    wordnet = WordNet(concepts=None)
     
-    def initSynsetKeyList(self):
-        wordnet = WordNet(concepts=None)
+    def init_synset_key_list(self):
+        
         synset_key_list = []
         
         for filename in os.listdir(self.ACTIONCORE_DB_PATH):
@@ -15,19 +17,44 @@ class ActionCoreDbCreator(object):
             file = open(os.path.join(self.ACTIONCORE_DB_PATH,filename),'r')
             content = file.read()
             regex_result = re.search('\s*//SYNSET_KEY\s*:\s*(\w+)',content)
-            print filename
+            
             if regex_result:
-                synset_key_list.append(wordnet.synsets(str(regex_result.group(1)),"v"))
+                synset_key_list.append(self.wordnet.synsets(str(regex_result.group(1)),"v"))
         
         return synset_key_list
     
     
+    def create_actioncore_dbs(self,input_dir):
+        #verb_tags = ['VB', 'VBG', 'VBZ', 'VBD', 'VBN', 'VBP', 'MD']
+        #Ignore modal verbs
+        verb_tags = ['VB', 'VBG', 'VBZ', 'VBD', 'VBN', 'VBP']
+        synset_key_list = ac.init_synset_key_list()
+        regex_has_pos = re.compile('has_pos\s*\(\s*(\w+)-{0,1}\d*\s*,\s*(\w+)\s*\)')
+        regex_new_db = re.compile("\n\s*-+\s*\n")
+         
+        for filename in os.listdir(input_dir):
+            if filename.endswith("~"): continue
+            
+            file = open(os.path.join(input_dir,filename),'r')
+            dbs = regex_new_db.split(file.read())
+            
+            for db in dbs:
+                for (word,tag) in regex_has_pos.findall(db):
+                    if tag in verb_tags:
+                        synset = self.wordnet.synsets(word,"v")
+
+                        if synset:
+                            print word
+                            print synset 
+                            print "---"
 
 if __name__ == '__main__':
-    ac = ActionCoreDbCreator()
-    list = ac.initSynsetKeyList()
+    args = sys.argv[1:]
     
-    print list
+    if args:
+        input_dir = args[0] 
+        ac = ActionCoreDbCreator()
+        ac.create_actioncore_dbs(input_dir)
             
             
     
