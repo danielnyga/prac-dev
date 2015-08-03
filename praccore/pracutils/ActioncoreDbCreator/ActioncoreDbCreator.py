c5cb38b6a38b476a1a68eb74325fdfee513f3020
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2015-08-03 14:55:02 +0200

    ActioncoreDbCreator creates actioncore dbs.

--- praccore/pracutils/ActioncoreDbCreator/ActioncoreDbCreator.py
+++ praccore/pracutils/ActioncoreDbCreator/ActioncoreDbCreator.py
@@ -7,7 +7,7 @@ class ActionCoreDbCreator(object):
     PRAC_HOME = os.environ['PRAC_HOME']
     ACTIONCORE_DB_PATH = os.path.join(PRAC_HOME, 'praccore', 'pracutils','ActioncoreDbCreator',"actioncore_dbs")
     wordnet = WordNet(concepts=None)
-    
+    SYNSET_KEY = "SYNSET_KEY"
     def init_synset_key_list(self):
         
         synset_key_list = []
@@ -16,11 +16,12 @@ class ActionCoreDbCreator(object):
             if filename.endswith("~"): continue
             file = open(os.path.join(self.ACTIONCORE_DB_PATH,filename),'r')
             content = file.read()
-            regex_result = re.search('\s*//SYNSET_KEY\s*:\s*(\w+)',content)
+            regex_result = re.search('\s*//'+self.SYNSET_KEY+'\s*:\s*(\w+)',content)
             
             if regex_result:
                 synset_key_list.append(self.wordnet.synsets(str(regex_result.group(1)),"v"))
-        
+            
+            file.close()
         return synset_key_list
     
     
@@ -44,10 +45,38 @@ class ActionCoreDbCreator(object):
                         synset = self.wordnet.synsets(word,"v")
 
                         if synset:
-                            print word
-                            print synset 
-                            print "---"
-
+                            is_synset_added = False
+                            
+                            for i in range(0,len(synset_key_list)):
+                                synset_key = synset_key_list[i]
+                                
+                                if self.are_synsets_equal(synset,synset_key):
+                                    file = open(os.path.join(self.ACTIONCORE_DB_PATH,str(i+1)+".db"),'a')
+                                    file.write('\n---\n')
+                                    file.write(self.SYNSET_KEY+":"+word+"\n")
+                                    file.write(db)
+                                    file.close()
+                                    is_synset_added = True
+                                    break
+                            
+                            if not is_synset_added:
+                                file = open(os.path.join(self.ACTIONCORE_DB_PATH,str(len(synset_key_list)+1)+".db"),'a')
+                                file.write(self.SYNSET_KEY+":"+word+"\n")
+                                file.write(db)
+                                file.close()
+                                synset_key_list.append(synset)
+                                
+                                
+    def are_synsets_equal(self,syn1,syn2):
+        
+        if len(syn1) != len(syn2):
+            return False
+        
+        for i in range(0,len(syn1)):
+            if not syn1[i] == syn2[i]:
+                return False
+            
+        return True 
 if __name__ == '__main__':
     args = sys.argv[1:]
     
