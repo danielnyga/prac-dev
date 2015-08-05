6754d92dbb05ad2d8d0d88ff811d50f9827e3204
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2015-08-05 13:26:59 +0200

    Updated semcore.py

--- praccore/pracutils/conv/semcore.py
+++ praccore/pracutils/conv/semcore.py
@@ -21,46 +21,79 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import lxml.html.soupparser as parser
+from mln.mln import Predicate
 from praclog import logging
 import os
+import sys
+from prac.core import PRAC
+from prac.wordnet import WordNet
 from nltk.corpus import wordnet
-from nlparsing import StanfordParser
 import java
+import mln.database
+from mln.errors import NoSuchPredicateError
+
 
 java.classpath.append(os.path.join('3rdparty', 'stanford-parser-2012-02-03', 'stanford-parser.jar'))
 grammarPath = os.path.join('3rdparty', 'stanford-parser-2012-02-03', 'grammar', 'englishPCFG.ser.gz')
+prac = PRAC()
+prac.wordnet = WordNet(concepts=None)
 
 def readSemcor3File(filename):
     '''
     Reads an XML semcore3.0 file and returns a corresponding MLN database.
     '''
+    print filename
     if not java.isJvmRunning():
             java.startJvm()
     log = logging.getLogger(__name__)
     tree = parser.parse(filename)
-    parserSt = StanfordParser(grammarPath)
+    
+    parserSt = prac.getModuleByName('nl_parsing')
+    parserSt.initialize()
+
+    
     target = open(filename+".db", 'w')
+    is_file_causing_parsing_errors = False
     for e in tree.iter():
         if e.tag == 's':
             s, atoms = reconstruct(e)
             target.write('//'+ s+"\n")
             for a in atoms:
                 target.write(a+"\n")
-            deps = parserSt.getDependencies(s)
-            depstr = map(str, deps)
-            # do some sanity check
-            for d in depstr:
-                target.write("1.00 " + d+"\n") 
-            target.write('---\n')
+            
+            is_sentence_parsed = False
+            
+            while not is_sentence_parsed:
+                try:
+                    for x in parserSt.parse_without_prac(s):
+                        x.write(target)
+                        target.write('---\n')
+                    is_sentence_parsed = True
+                    print "Parsing successfully."
+                except NoSuchPredicateError:
+                    _, exc_value , _ = sys.exc_info()
+                    print exc_value
+                    predicate_name = str(exc_value).split(':')[1].strip()
+                    parserSt.mln.declarePredicate(Predicate(predicate_name,['word','word']))
+                    
+                    print "Try it again."
+                except:
+                    is_sentence_parsed = True
+                    is_file_causing_parsing_errors = True
+                    print sys.exc_info()[0]
+                
     target.close()
+    if not is_file_causing_parsing_errors:
+        os.remove(filename)
 
 def reconstruct(s_element):
     sentence = []
-    wf_count = 1
+    wf_count = 0
     gnd_atoms = []
     for e in s_element.iter():
         if e.text is not None:
             sentence.append(e.text)
+	
         if e.tag == 'wf':
             wf_count += 1
             word_const = '%s-%d' % (e.text, wf_count)
@@ -76,10 +109,21 @@ def reconstruct(s_element):
                 if synset is not None:
                     gnd_atoms.append('1.00 has_sense(%s,%s)' % (word_const, synset.name))
                     gnd_atoms.append('1.00 is_a(%s,%s)' % (synset.name, synset.name))
+        
+        elif e.tag == 'punc':
+            wf_count += 1
+
     sentence = ' '.join(sentence).strip()
     return sentence, gnd_atoms
     
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     path = os.path.join('/', 'home', 'seba', 'Desktop', 'semcor3.0', 'brown1', 'tagfiles')
-    readSemcor3File(os.path.join(path, 'br-a02'))
+    path2 = os.path.join('/', 'home', 'seba', 'Desktop', 'semcor3.0', 'brown2', 'tagfiles')
+    path3 = os.path.join('/', 'home', 'seba', 'Desktop', 'semcor3.0', 'brownv', 'tagfiles')
+    for filename in os.listdir(path):
+        readSemcor3File(os.path.join(path, filename))
+    #for filename in os.listdir(path2):
+    #    readSemcor3File(os.path.join(path2, filename))
+    #for filename in os.listdir(path3):
+    #    readSemcor3File(os.path.join(path3, filename))
