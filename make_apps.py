3bbb7b27339bebe622f2531cf7a553d79427d646
Mareike <mareikep@tzi.de>
2015-08-25 10:48:05 +0200

    remove -O flag in make-apps to fix wordnet error caused by optimization

--- make_apps.py
+++ make_apps.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         print "  %s" % filename
         f = file(filename, "w")
         f.write(preamble)
-        f.write("python -O \"%s\" %s\n" % (adapt(app["script"], arch), allargs))
+        f.write("python \"%s\" %s\n" % (adapt(app["script"], arch), allargs))
         f.close()
         if not isWindows: os.chmod(filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
 
