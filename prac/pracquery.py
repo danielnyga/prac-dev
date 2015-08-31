b96a8270d975e4914384ff36fccf89c978cbac1d
Mareike <mareikep@tzi.de>
2015-08-31 15:56:14 +0200

    remove urwid package from 3rdparty, rename and merge querytool

--- prac/pracquery.py
+++ prac/pracquery.py
@@ -37,9 +37,12 @@ from Tkconstants import BOTH, W, LEFT, NE, E
 # --- inference class ---
 from prac.core import PRACKnowledgeBase
 from prac.core.base import PRAC
+from prac.core.inference import PRACInference
+from prac.core.wordnet import WordNet
+from prac.pracutils.RolequeryHandler import RolequeryHandler
 from pracmln import MLN, Database
 from pracmln.mln.methods import InferenceMethods
-from pracmln.mln.util import balancedParentheses, ifNone
+from pracmln.mln.util import balancedParentheses, ifNone, colorize
 from pracmln.praclog import logger
 from pracmln.utils.config import query_config_pattern, PRACMLNConfig
 from pracmln.utils.widgets import FilePickEdit
@@ -560,11 +563,82 @@ class PRACQueryGUI(object):
 
 # -- main app --
 
+
+
 if __name__ == '__main__':
-    # read previously saved settings
-    settings = {}
-    
+
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-i", "--interactive", dest="interactive", default=False, action='store_true',
+                      help="Starts PRAC inference with an interactive GUI tool.")
+    (options, args) = parser.parse_args()
+
+    interactive = options.interactive
+
+    sentences = args
+
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
+    actionRoles = None
     prac = PRAC()
-    app = PRACQueryGUI(os.path.join(prac.getManifestByName('nl_parsing').module_path, 'bin'), settings)
-    
+    prac.wordnet = WordNet(concepts=None)
+
+    infer = PRACInference(prac, sentences)
+#     actionCore.insertdbs(infer, *readDBFromString(prac.mln, dbs))
+
+    if interactive: # use the GUI
+        # in case we have natural-language parameters, parse them
+        if len(infer.instructions) > 0:
+            parser = prac.getModuleByName('nl_parsing')
+            prac.run(infer, parser)
+        conf = PRACMLNConfig()
+        gui = PRACQueryGUI(infer, conf)
+        gui.open()
+    else: # regular PRAC pipeline
+        while infer.next_module() != None :
+            modulename = infer.next_module()
+            module = prac.getModuleByName(modulename)
+            prac.run(infer,module)
+#             if modulename == 'senses_and_roles':
+#                 module.role_distributions(infer)
+#                 exit(0)
+
+
+    step = infer.inference_steps[-1]
+    print
+    print colorize('+========================+',  (None, 'green', True), True)
+    print colorize('| PRAC INFERENCE RESULTS |',  (None, 'green', True), True)
+    print colorize('+========================+',  (None, 'green', True), True)
+    wordnet_module = prac.getModuleByName('wn_senses')
+
+
+#             db.write(sys.stdout, color=True)
+    for db in step.output_dbs:
+        for a in sorted(db.evidence.keys()):
+            v = db.evidence[a]
+            if v > 0.001 and (a.startswith('action_core') or a.startswith('has_sense') or a.startswith('achieved_by')):
+                if a.startswith('has_sense'):
+
+                    group = re.split(',',re.split('has_sense\w*\(|\)',a)[1])
+                    word = group[0];
+                    sense = group[1];
+                    if sense != 'null':
+                        print
+                        print colorize('  WORD:', (None, 'white', True), True), word,
+                        print colorize('  SENSE:', (None, 'white', True), True), sense
+                        wordnet_module.printWordSenses(wordnet_module.get_possible_meanings_of_word(db, word), sense)
+                        print
+                else:
+                    print '%.3f    %s' % (v, a)
+        RolequeryHandler.queryRolesBasedOnActioncore(db).write(color=True)
+
+    if hasattr(step, 'executable_plans'):
+        print
+        print colorize('+==========================+',  (None, 'green', True), True)
+        print colorize('| PARAMETERIZED ROBOT PLAN |',  (None, 'green', True), True)
+        print colorize('+==========================+',  (None, 'green', True), True)
+        print
+        for plan in step.executable_plans:
+            print plan
+
 
