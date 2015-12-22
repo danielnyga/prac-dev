03aa985ebba8d15b6140180d2d87f305990ce2b0
Mareike <mareikep@tzi.de>
2015-12-22 10:13:53 +0100

    update run.py for ssl support

--- pracweb/run.py
+++ pracweb/run.py
@@ -1,5 +1,8 @@
 from pracweb.gui.app import pracApp
 import logging
+import os
+from werkzeug.serving import run_simple
+from OpenSSL import SSL
 
 def init_app(app):
 
@@ -18,4 +21,11 @@ init_app(pracApp.app)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    pracApp.app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
+    if 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'true':
+        context = SSL.Context(SSL.SSLv23_METHOD)
+        context.use_privatekey_file(os.path.join('../../', 'default.key'))
+        context.use_certificate_file(os.path.join('../../', 'default.crt'))
+        run_simple('0.0.0.0', 5002, pracApp.app, ssl_context=context)
+    else:
+        pracApp.app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
+
