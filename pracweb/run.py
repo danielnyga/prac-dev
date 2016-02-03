c52719cc3efd0ee0e86887305c9ae81dbb0e81e1
Mareike <mareikep@tzi.de>
2016-02-03 12:21:47 +0100

    update start routine

--- pracweb/run.py
+++ pracweb/run.py
@@ -21,7 +21,7 @@ init_app(pracApp.app)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
-    if 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'true':
+    if 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'deploy':
         log.debug('Running PRACWEB in server mode')
 
         # load config
@@ -30,11 +30,17 @@ if __name__ == '__main__':
         http_server = HTTPServer(WSGIContainer(pracApp.app))
         http_server.listen(5001)
         IOLoop.instance().start()
+    elif 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'testing':
+        log.debug('Running PRACWEB in testing mode')
 
+        # load config
+        pracApp.app.config.from_object('configmodule.TestingConfig')
+
+        pracApp.app.run(host='0.0.0.0', port=5001)
     else:
         log.debug('Running PRACWEB in development mode')
 
         # load config
         pracApp.app.config.from_object('configmodule.DevelopmentConfig')
 
-        pracApp.app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
+        pracApp.app.run(host='0.0.0.0', port=5001)
