80ced0c40559c9c4c80a64c2cd936ab1f73dea3a
Sebastian Koralewski <koralews@informatik.uni-bremen.de>
2016-03-03 16:14:07 +0100

    Instruction will be stored in mongo db.

--- prac/pracutils/InstructionProcessor.py
+++ prac/pracutils/InstructionProcessor.py
@@ -4,6 +4,35 @@ from prac.core.inference import PRACInference
 from prac.core.wordnet import WordNet
 import sys
 from RolequeryHandler import RolequeryHandler
+import json
+from pymongo import MongoClient,errors
+
+def store_instruction_in_mongo_db(text_file_name,action_core,roles_dict,plan_list):
+    json_str="""
+                    {{ 
+                            "_id" : "{}",
+                            "action_core" : "{}",
+                            "action_roles" : {},
+                            "plan_list" : {}
+                    }}
+                    """.format(text_file_name,
+                               action_core,
+                               str(roles_dict),
+                               str(plan_list))
+    
+    mongo_client = MongoClient()
+    ies_mongo_db = mongo_client.PRAC
+    collection = ies_mongo_db.Instructions
+    
+    try:
+        json_dict = {"_id" : text_file_name, "action_core" : action_core, "action_roles" : roles_dict, "plan_list" : plan_list}
+        collection.insert_one(json_dict)
+        
+    except errors.DuplicateKeyError:
+        collection.delete_many({"_id" : json_dict['_id']})
+        collection.insert_one(json_dict)
+        
+    mongo_client.close()
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -22,13 +51,14 @@ if __name__ == '__main__':
     
     for text_file_path in corpus:    
         text_file = open(text_file_path, 'r')
+        text_file_name = path.basename(text_file_path)
         sentences = text_file.readlines()
         
         prac = PRAC()
         prac.wordnet = WordNet(concepts=None)
             
         #Parse text file name to annotate it in the mongo db
-        inference = PRACInference(prac, ["{}.".format(path.basename(text_file_path))])
+        inference = PRACInference(prac, ["{}.".format(text_file_name)])
         while inference.next_module() != 'achieved_by' and inference.next_module() != 'plan_generation':
             modulename = inference.next_module()
             module = prac.getModuleByName(modulename)
@@ -52,4 +82,4 @@ if __name__ == '__main__':
             if hasattr(step, 'executable_plans'):
                 plan_list.extend(step.executable_plans)
                 
-        print plan_list            
+        store_instruction_in_mongo_db(text_file_name, "Flipping", roles_dict, plan_list)
