27f707532586d1a669481a5767b607ca4c8c8057
Susanne Knoop <susanne.knoop@gmail.com>
2016-03-07 19:20:51 +0100

    send ROS Action Cores to CRAM server

--- pracweb/gui/pages/cram.py
+++ pracweb/gui/pages/cram.py
@@ -52,34 +52,36 @@ def execute_plan():
     t = Thread(target=_execute_plan, args=(pracsession, 180, method, data))
     t.start()
     pracsession.infbuffer.waitformsg()
-    pracsession.log.info('received msg: %s' %pracsession.infbuffer.content) #TODO ist noch falsch
+    pracsession.log.info('received msg: %s' %pracsession.infbuffer.content)
     return jsonify(pracsession.infbuffer.content)
 
 
 def _execute_plan(pracsession, timeout, method, data):
 
-    #pracsession.log.info('executing plan')
+    pracsession.log.info('executing plan...')
     pracsession.log.info(data)
 
     plan = data['plan']
 
-    # TODO fill in real actionroles from inference
-    core1 = ActionCore()
-    core1.action_core_name = 'Starting'
-    core1.action_roles = [ActionRole(role_name='obj_to_be_started', role_value='centrifuge.n.01')]
-    action_cores = [core1]
+    print pracsession.actioncores
+    ros_actioncores = []
+
+    for ac in pracsession.actioncores:
+        ros_actioncores.append(ActionCore(ac['action_core_name'], [ActionRole(**r) for r in ac['action_roles']]))
+
+    print ros_actioncores
 
     try:
         rospy.wait_for_service('prac2cram', timeout=5)
-
+    # called when timeout exceeded
     except rospy.ROSException, e:
         pracsession.log.error(e)
         pracsession.infbuffer.setmsg({'status': -1, 'message': 'Timeout exceeded. Please start the ROS service "Prac2Cram" and try again.'})
 
     try:
-        # create a handle to the service
+        # create a handle to the ROS service
         prac2cram = rospy.ServiceProxy('prac2cram', Prac2Cram)
-        resp = prac2cram(action_cores, plan)
+        resp = prac2cram(ros_actioncores, plan)
         pracsession.log.info('Response: %s' %resp)
         if resp.status: # if error
             message = 'The CRAM service request failed!'
@@ -88,5 +90,5 @@ def _execute_plan(pracsession, timeout, method, data):
         pracsession.infbuffer.setmsg({'status': resp.status, 'message': message})
 
     except rospy.ServiceException, e:
-        print "Service call failed with the following error: %s" %e
+        log.error("Service call failed with the following error: {}".format(e.message))
         return
