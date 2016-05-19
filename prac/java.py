190e6bfbe5a17c387d236106a1b0e62b2cd37185
Mareike <mareikep@tzi.de>
2016-05-19 10:54:49 +0200

    memory allocation java

--- prac/java.py
+++ prac/java.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Apr 21, 2012
 
 @author: nyga
-'''
+"""
 import jpype
 import platform
 import logging
@@ -10,26 +10,25 @@ import logging
 arch = {'x86_64': 'amd64', 'i686': 'i386'}
 classpath = []
 
+
 def startJvm():
     import os
-    #print stanford_parser_home
     java_home = os.environ['JAVA_HOME']
     machine_arch = platform.machine()
-#    print  machine_arch
     if machine_arch not in arch.keys():
         raise Exception('Your system architecture is not supported: %s' % machine_arch)
     java_home += '/jre/lib/%s/libjava.so' % arch[machine_arch]   
-    #print java_home
-    #print 'classpath=', classpath
     log = logging.getLogger('java')
     log.debug(jpype.getDefaultJVMPath())
-    jpype.startJVM(jpype.getDefaultJVMPath(), '-ea', '-Djava.class.path=%s' % (':'.join(classpath)))
     print 'starting JVM...'
-#    jpype.startJVM(java_home, '-ea', '-Djava.class.path=%s' % (':'.join(classpath)))
+    jpype.startJVM(jpype.getDefaultJVMPath(), '-Xmx512m', '-Xms512m', '-ea',
+                   '-Djava.class.path=%s' % (':'.join(classpath)))
     print 'JVM running'
-    
+
+
 def shutdownJvm():
     jpype.shutdownJVM()
-    
+
+
 def isJvmRunning():
     return jpype.isJVMStarted()
