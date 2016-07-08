7096df9167b0be82318f041cd10cb7b93ea98ef5
Daniel Nyga <nyga@cs.uni-bremen.de>
2016-07-08 13:37:30 +0200

    added locations module

---
+++ prac/core/locations.py
@@ -0,0 +1,29 @@
+# 
+#
+# (C) 2011-2016 by Daniel Nyga (nyga@cs.uni-bremen.de)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+
+_home = __file__.split(os.path.sep)[:-3]
+home = os.path.sep.join(_home)
+thirdparty = os.path.sep.join(_home + ['3rdparty'])
+modules = os.path.sep.join(_home + ['pracmodules'])
