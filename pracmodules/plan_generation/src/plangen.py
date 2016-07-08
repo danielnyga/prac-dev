05760d2db62197b8362dae8ad7c6413058b3b932
Mareike <mareikep@tzi.de>
2016-07-08 17:21:17 +0200

    update plan generation (add properties)

--- pracmodules/plan_generation/src/plangen.py
+++ pracmodules/plan_generation/src/plangen.py
@@ -20,53 +20,94 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-import copy
+import os
+import itertools
 from prac.core.base import PRACModule
 from prac.core.inference import PRACInferenceStep
-from pracmln.mln.util import out
+from pracmln import MLN
+from pracmln.mln.util import colorize, out
 from pracmln.praclog import logger
 
-log = logger(__name__)
+log_ = logger(__name__)
 
 
 class PlanGenerator(PRACModule):
 
     def __call__(self, pracinference, **params):
-        out('generating CRAM plan')
+
+        log_.info('Running {}'.format(self.name))
+
+        print colorize('+=========================+', (None, 'green', True), True)
+        print colorize('| GENERATING CRAM PLAN(S) |', (None, 'green', True), True)
+        print colorize('+=========================+', (None, 'green', True), True)
+        print
+
         infstep = PRACInferenceStep(pracinference, self)
         dbs = pracinference.inference_steps[-1].output_dbs
         infstep.output_dbs = dbs
         infstep.inferred_roles = {}
         infstep.executable_plans = []
-        
+        properties = [p.name for p in MLN.load(os.path.join(self.module_path, '..', 'prop_extraction', 'mln', 'predicates.mln')).predicates]
+
         for db in dbs:
-            for query in ('achieved_by(?ac1, ?ac)', 'action_core(?w, ?ac)'):
+            for query in ['achieved_by(?ac1, ?ac)', 'action_core(?w, ?ac)']:
                 for q in db.query(query):
                     actioncore = q['?ac']
                     ac = self.prac.actioncores.get(actioncore)
                     if not ac.plan:
                         continue
-                    assignment = {}
-                    role_assignments = [assignment]
-                    out('roles: ')
-                    out(ac.roles)
 
+                    roleass = {}
+                    role_assignments = []
                     for role in ac.roles:
-                        for i, rq in enumerate(db.query('{0}(?w,{1}) ^ has_sense(?w, ?s)'.format(role, actioncore))):
-                            if i > 0:
-                                for ass in list(role_assignments):
-                                    new_ass = copy.copy(ass)
-                                    role_assignments.append(new_ass)
-                                    new_ass[role] = rq['?s']
-                            else:
-                                for ass in role_assignments:
-                                    ass[role] = rq['?s']
-                        # Handle missing plan parameters
-                        if role not in ass.keys():
-                            ass[role] = "Unknown"
+                        if role == 'action_verb': continue
+                        roleass[role] = list(db.query('{0}(?w,{1}) ^ has_sense(?w, ?s)'.format(role, actioncore)))
+
+                    emptyroles = []
+
+                    # remove empty roles
+                    for r in roleass.keys():
+                        if not roleass[r]:
+                            emptyroles.append(r)
+                            del roleass[r]
+
+                    # itertools.product will return [] if roleass contains
+                    # roles with empty lists, therefore we removed them
+                    # before and re-add them later
+                    combis = list((dict(itertools.izip(roleass, x)) for x in itertools.product(*roleass.itervalues())))
+
+                    for c in combis:
+
+                        # re-add empty roles
+                        for e in emptyroles:
+                            c[e] = {}
+
+                        ass = {}
+                        for role in c:
+                            word = c[role].get('?w', 'Unknown')
+                            sense = c[role].get('?s', 'Unknown')
+                            ass[role] = sense
+
+                            # add properties to the objects of the according roles
+                            for prop in properties:
+                                for rq2 in db.query('{0}({1},?w2) ^ has_sense(?w2, ?s2)'.format(prop, word)):
+                                    if '{}_props'.format(role) in ass:
+                                        ass['{}_props'.format(role)] += ' ({} {})'.format(prop, rq2['?s2'])
+                                    else:
+                                        ass['{}_props'.format(role)] = ' ({} {})'.format(prop, rq2['?s2'])
+
+                            if '{}_props'.format(role) not in ass:
+                                ass['{}_props'.format(role)] = ''
+
+                            # Handle missing plan parameters
+                            if role not in ass.keys():
+                                ass[role] = "Unknown"
+
+                        role_assignments.append(ass)
+
                     for assignment in role_assignments:
                         infstep.inferred_roles[ac.name] = assignment
-                        infstep.executable_plans.append(
-                            ac.parameterize_plan(**assignment))
+                        infstep.executable_plans.append(ac.parameterize_plan(**assignment))
                     break
+        print infstep.executable_plans
         return infstep
