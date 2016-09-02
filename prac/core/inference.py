e40c584c7f3440dcd50516570815a399aaf93c58
Mareike <mareikep@cs.uni-bremen.de>
2016-09-02 10:20:18 +0200

    add separate repr function for fringe text in pracweb

--- prac/core/inference.py
+++ prac/core/inference.py
@@ -246,7 +246,11 @@ class FrameNode(PRACInferenceNode):
 
     def __str__(self):
         return str(self.frame)
-        
+
+
+    def repstr(self):
+        return self.frame.repstr()
+
 
 class PRACInference(object):
     '''
